9519af33176e9df8ca8e92046128b6f643d48672
Jan Gosmann <jan.gosmann@tngtech.com>
2019-01-12 20:06:26 +0100

    Simplify example.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { Scrollbars } from 'react-custom-scrollbars'
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{width: '99%'}}>
-          <Scrollbars autoHeight autoHeightMin={0} autoHeightMax='100%' style={{backgroundColor: 'blue'}}>
-            <div style={{width: '200px', height: '200px', backgroundColor: 'red'}} />
-          </Scrollbars>
+      <div className="App">
+        <Scrollbars autoHeight>
+            content
+        </Scrollbars>
       </div>
     );
   }
